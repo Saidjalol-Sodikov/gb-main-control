721da17ad41ec08289e416d626326947c14693a2
Saidjalol Sodikov <ssodikovu@gmail.com>
2024-01-26 02:10:38 +0500

    Вывел основной алгоритм программы в метод.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,36 @@
 ﻿//Для улучшения читаемости кода названия переменных были изменены
-Console.Clear();
-string[] a = ["abc", "1234", ";-)", "q2w"];
-int dlinaKonechnogoMassiva = 0;
-for (int i = 0; i < a.Length; i++)
-{
-    if (a[i].Length<=3) dlinaKonechnogoMassiva++;
-}
-string[] konechniyMassiv = new string[dlinaKonechnogoMassiva];
-int ideksTekushegoMassiva = 0;
-for (int i = 0; i < a.Length; i++)
-{
-    if (a[i].Length<=3){
-        konechniyMassiv[ideksTekushegoMassiva]=a[i];
-        ideksTekushegoMassiva++;
+//Программу превратил в метод и добавил возможность вычленять разные размеры
+
+
+//Основной метод программы
+string[] KonechniyMassiv (string[] a, int n){
+    int dlinaKonechnogoMassiva = 0;
+    for (int i = 0; i < a.Length; i++)
+    {
+        if (a[i].Length<=n) dlinaKonechnogoMassiva++;
     }
+    string[] konechniyMassiv = new string[dlinaKonechnogoMassiva];
+    int ideksTekushegoMassiva = 0;
+    for (int i = 0; i < a.Length; i++)
+    {
+        if (a[i].Length<=n){
+            konechniyMassiv[ideksTekushegoMassiva]=a[i];
+            ideksTekushegoMassiva++;
+        }
+    }
+    return konechniyMassiv;
 }
 
-Console.WriteLine($"Исходный массив: [\"{string.Join("\", \"", a)}\"]");
-Console.WriteLine($"Конечныйый массив: [\"{string.Join("\", \"", konechniyMassiv)}\"]");
+//Очищаем терминал
+Console.Clear();
+
+//Начальные данные
+string[] ishodniyMassiv = ["abc", "1234", ";-)", "q2w", "ac", "12354", ";)", "q2uuw"];
+int limitStroki = 3;
+
+//Находим решение
+string[] vihodnoyMassiv = KonechniyMassiv(ishodniyMassiv, limitStroki);
+
+//Выводим результат
+Console.WriteLine($"Исходный массив: [\"{string.Join("\", \"", ishodniyMassiv)}\"]");
+Console.WriteLine($"Конечныйый массив: [\"{string.Join("\", \"", vihodnoyMassiv)}\"]");
