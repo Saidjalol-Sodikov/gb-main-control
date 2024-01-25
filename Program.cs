7ae6c1312e95b99863145571f1fe29137dbec104
Saidjalol Sodikov <ssodikovu@gmail.com>
2024-01-26 01:51:40 +0500

    Создал программу по блок-схеме алгоритма

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Для улучшения читаемости кода названия переменных были изменены
+Console.Clear();
+string[] a = ["abc", "1234", ";-)", "q2w"];
+int dlinaKonechnogoMassiva = 0;
+for (int i = 0; i < a.Length; i++)
+{
+    if (a[i].Length<=3) dlinaKonechnogoMassiva++;
+}
+string[] konechniyMassiv = new string[dlinaKonechnogoMassiva];
+int ideksTekushegoMassiva = 0;
+for (int i = 0; i < a.Length; i++)
+{
+    if (a[i].Length<=3){
+        konechniyMassiv[ideksTekushegoMassiva]=a[i];
+        ideksTekushegoMassiva++;
+    }
+}
+
+Console.WriteLine($"Исходный массив: [\"{string.Join("\", \"", a)}\"]");
+Console.WriteLine($"Конечныйый массив: [\"{string.Join("\", \"", konechniyMassiv)}\"]");
